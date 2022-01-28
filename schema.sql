27fa95f7450ea76ff432672887d39b15f72a874b
tommijuslin <tommi.juslin@gmail.com>
2022-01-28 08:48:05 +0200

    update db schema

--- schema.sql
+++ schema.sql
@@ -4,3 +4,40 @@ CREATE TABLE users (
     password TEXT,
     role INTEGER
 );
+
+CREATE TABLE games (
+    id SERIAL PRIMARY KEY,
+    title TEXT
+);
+
+CREATE TABLE platforms (
+    id SERIAL PRIMARY KEY,
+    name TEXT
+);
+
+CREATE TABLE games_platforms (
+    id SERIAL PRIMARY KEY,
+    game_id INTEGER REFERENCES games,
+    platform_id INTEGER REFERENCES platforms
+);
+
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name TEXT
+);
+
+CREATE TABLE games_categories (
+    id SERIAL PRIMARY KEY,
+    game_id INTEGER REFERENCES games,
+    category_id INTEGER REFERENCES categories
+);
+
+CREATE TABLE runs (
+    id SERIAL PRIMARY KEY,
+    time REAL,
+    date TIMESTAMP,
+    user_id INTEGER REFERENCES users,
+    game_id INTEGER REFERENCES games,
+    platform_id INTEGER REFERENCES platforms,
+    category_id INTEGER REFERENCES categories
+);
