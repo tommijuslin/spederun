d554831c2c9f76c22b0ea56dd863600abcdefdc7
tommijuslin <tommi.juslin@gmail.com>
2022-01-28 10:22:17 +0200

    add individual pages for games

--- games.py
+++ games.py
@@ -8,3 +8,7 @@ def add_game(title):
 def get_all_games():
     sql = "SELECT id, title FROM games ORDER BY title"
     return db.session.execute(sql).fetchall()
+
+def get_game(game_id):
+    sql = "SELECT id, title FROM games WHERE id=:game_id"
+    return db.session.execute(sql, {"game_id":game_id}).fetchone()
--- routes.py
+++ routes.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template, request, redirect, session
+from werkzeug.security import check_password_hash, generate_password_hash
 import games
 
 @app.route("/")
@@ -16,6 +17,10 @@ def add_game():
         games.add_game(title)
     
         return redirect("/")
+
+@app.route("/game/<int:id>")
+def game(id):
+    return render_template("game.html", game=games.get_game(id))
 
 @app.route("/login", methods=["get", "post"])
 def login():
