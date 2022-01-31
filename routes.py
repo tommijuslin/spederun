3b57d2375debbf791006b590c598ba587a76b60a
tommijuslin <tommi.juslin@gmail.com>
2022-01-31 13:08:51 +0200

    add registration

--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template, request, redirect, session, url_for
-from werkzeug.security import check_password_hash, generate_password_hash
+import users
 import games
 import runs
 import platforms
@@ -42,18 +42,36 @@ def game(id):
                                         runs=runs.get_runs(id),
                                         platforms=games_platforms.get_all_platforms(id))
 
-@app.route("/login", methods=["get", "post"])
-def login():        
+@app.route("/login", methods=["GET", "POST"])
+def login():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        session["username"] = username
-
-        return redirect("/")
+        if users.login(username, password):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Wrong username or password")
     
     return render_template("login.html")
 
+@app.route("/register", methods=["get", "post"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            return render_template("error.html", message="Passwords don't match")
+        if users.register(username, password1):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Registration failed")
+    
+    return render_template("register.html")
+    
+
 @app.route("/logout")
 def logout():
+    del session["user_id"]
     del session["username"]
     return redirect("/")
